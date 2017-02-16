a1d7bc0e0bba87c9621734425b2fd74e0b72fe46
Steven Blowers <sblowers@findmypast.com>
2017-02-16 09:05:36 +0000

    initial mix setup with supervisor

---
+++ lib/mssqlex.ex
@@ -0,0 +1,18 @@
+defmodule Mssqlex do
+  @moduledoc """
+  Documentation for Mssqlex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mssqlex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mssqlex_test.exs
@@ -0,0 +1,8 @@
+defmodule MssqlexTest do
+  use ExUnit.Case
+  doctest Mssqlex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
