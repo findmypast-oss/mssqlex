febd16c870470ac4c48f09d177d5dfaefff06540
Steven Blowers <steven@blowers.me>
2017-02-20 20:57:39 +0000

    setting up config for ExDoc

--- mix.exs
+++ mix.exs
@@ -7,28 +7,22 @@ defmodule Mssqlex.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+
+     # Docs
+     name: "Mssqlex",
+     source_url: "https://github.com/findmypast/mssqlex",
+     docs: [main: "Mssqlex",
+            extras: ["README.md"]]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger, :odbc],
      mod: {Mssqlex.Application, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:db_connection, "~> 1.1"}]
+    [{:db_connection, "~> 1.1"},
+     {:ex_doc, "~> 0.15", only: :dev, runtime: false}]
   end
 end
