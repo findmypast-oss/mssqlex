b26a0672ef53183d81cb66e98a79cadf525d09ad
Jae Bach Hardie <jhardie@findmypast.com>
2017-03-03 16:53:16 +0000

    Further refactoring

--- test/mssqlex/login_test.exs
+++ test/mssqlex/login_test.exs
@@ -5,13 +5,15 @@ defmodule Mssqlex.LoginTest do
 
   test "Given valid details, connects to database" do
     assert {:ok, pid} = Mssqlex.start_link([])
-    assert {:ok, _, %Result{num_rows: 1, rows: [["test"]]}} = Mssqlex.query(pid, "SELECT 'test'", [])
+    assert {:ok, _, %Result{num_rows: 1, rows: [["test"]]}} =
+      Mssqlex.query(pid, "SELECT 'test'", [])
   end
 
   test "Given invalid details, errors" do
     Process.flag(:trap_exit, true)
 
     assert {:ok, pid} = Mssqlex.start_link(password: "badpass")
-    assert_receive {:EXIT, ^pid, %Mssqlex.Error{odbc_code: :invalid_authorization}}
+    assert_receive {:EXIT, ^pid,
+                    %Mssqlex.Error{odbc_code: :invalid_authorization}}
   end
 end
--- test/mssqlex/query_test.exs
+++ test/mssqlex/query_test.exs
@@ -12,24 +12,28 @@ defmodule Mssqlex.QueryTest do
   end
 
   test "simple select", %{pid: pid} do
-    assert {:ok, _, %Result{}} =
-      Mssqlex.query(pid, "CREATE TABLE query_test.dbo.simple_select (name varchar(50));", [])
+    assert {:ok, _, %Result{}} = Mssqlex.query(pid,
+      "CREATE TABLE query_test.dbo.simple_select (name varchar(50));", [])
 
-    assert {:ok, _, %Result{num_rows: 1}} =
-      Mssqlex.query(pid, ["INSERT INTO query_test.dbo.simple_select VALUES ('Steven');"], [])
+    assert {:ok, _, %Result{num_rows: 1}} = Mssqlex.query(pid,
+      ["INSERT INTO query_test.dbo.simple_select VALUES ('Steven');"], [])
 
-    assert {:ok, _, %Result{num_rows: 1, rows: [["Steven"]]}} =
-      Mssqlex.query(pid, "SELECT * FROM query_test.dbo.simple_select;", [])
+    assert {:ok, _, %Result{num_rows: 1, rows: [["Steven"]]}} = Mssqlex.query(
+      pid, "SELECT * FROM query_test.dbo.simple_select;", [])
   end
 
   test "parametrized queries", %{pid: pid} do
-    assert {:ok, _, %Result{}} =
-      Mssqlex.query(pid, "CREATE TABLE query_test.dbo.parametrized_query (id int, name varchar(50), joined datetime2);", [])
+    assert {:ok, _, %Result{}} = Mssqlex.query(pid,
+      "CREATE TABLE query_test.dbo.parametrized_query" <>
+      "(id int, name varchar(50), joined datetime2);", [])
 
-    assert {:ok, _, %Result{num_rows: 1}} =
-      Mssqlex.query(pid, ["INSERT INTO query_test.dbo.parametrized_query VALUES (?, ?, ?);"], [1, "Jae", "2017-01-01 12:01:01.3450000"])
+    assert {:ok, _, %Result{num_rows: 1}} = Mssqlex.query(pid,
+      ["INSERT INTO query_test.dbo.parametrized_query VALUES (?, ?, ?);"],
+      [1, "Jae", "2017-01-01 12:01:01.3450000"])
 
-    assert {:ok, _, %Result{num_rows: 1, rows: [[1, "Jae", {{2017, 1, 1}, {12, 1, 1, 0}}]]}} =
+    assert {:ok, _, %Result{
+               num_rows: 1,
+               rows: [[1, "Jae", {{2017, 1, 1}, {12, 1, 1, 0}}]]}} =
       Mssqlex.query(pid, "SELECT * FROM query_test.dbo.parametrized_query;", [])
   end
 end
