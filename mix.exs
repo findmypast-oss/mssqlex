6b5715f68d352cd7606d52fdca112fd2de34b087
Steven Blowers <sblowers@findmypast.com>
2017-03-15 13:50:59 +0000

    attempting to run inch ci after travis build

--- mix.exs
+++ mix.exs
@@ -33,7 +33,8 @@ defmodule Mssqlex.Mixfile do
     [{:db_connection, "~> 1.1"},
      {:decimal, "~> 1.0"},
      {:ex_doc, "~> 0.15", only: :dev, runtime: false},
-     {:excoveralls, "~> 0.6", only: :test}]
+     {:excoveralls, "~> 0.6", only: :test},
+     {:inch_ex, "~> 0.5", only: :docs}]
   end
 
   defp package do
