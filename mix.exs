f962ce1ac510067fbb2af9f5a246a1ead84e42fd
Steven Blowers <shdblowers@gmail.com>
2017-08-27 19:15:27 +0100

    Adding exfmt package

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule Mssqlex.Mixfile do
      {:decimal, "~> 1.0"},
      {:ex_doc, "~> 0.15", only: :dev, runtime: false},
      {:excoveralls, "~> 0.6", only: :test},
-     {:inch_ex, "~> 0.5", only: :docs}]
+     {:inch_ex, "~> 0.5", only: :docs},
+     {:exfmt, "~> 0.4.0", only: :dev}]
   end
 
   defp package do
