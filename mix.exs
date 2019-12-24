3cf4b66c0aef0046b4e7020da6b064af54efff21
Jae Anne Bach Hardie <jhardie@findmypast.com>
2019-12-24 11:37:59 +0000

    Update exdocs version for Elixir 1.9 compatibility

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Mssqlex.Mixfile do
       {:db_connection, "~> 2.1"},
       {:decimal, "~> 1.8"},
       {:gproc, "~> 0.8"},
-      {:ex_doc, "~> 0.15", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:excoveralls, "~> 0.6", only: :test},
       {:inch_ex, "~> 0.5", only: :docs},
       {:exfmt, "~> 0.4.0", only: :dev}
